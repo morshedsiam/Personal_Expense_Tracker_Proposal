d9123834be31cf960c8a379b033ff2eac71718ca
morshedsiam <morshedsiam66@gmail.com>
2026-05-24 18:41:34 +0600

    Added Main.java is added with Login Screen Class

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package personalexpensetracker;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+        LoginScreen loginScreen = new LoginScreen(primaryStage);
+        loginScreen.show();
+    }
+
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        launch(args);
     }
 }
