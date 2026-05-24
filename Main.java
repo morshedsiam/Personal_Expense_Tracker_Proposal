c56a0914c43699dff73f697c5c8269ffe4ca6cd3
morshedsiam <morshedsiam66@gmail.com>
2026-05-24 18:45:24 +0600

    Added Main.java is added with Login Screen (Run file)

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        
+        //This is the running file (Siam)
         LoginScreen loginScreen = new LoginScreen(primaryStage);
         loginScreen.show();
     }
