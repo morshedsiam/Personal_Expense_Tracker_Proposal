208b3d2ddcf17fb53fd009c791fab18b0fdb6a8c
morshedsiam <morshedsiam66@gmail.com>
2026-05-24 18:36:37 +0600

    Added Main.java is added with Login Screen Class

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personalexpensetracker;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+    }
+}
